cc5a411c25b15e0239dff733244a9575f1429b44
namthang <namthang161295@users.noreply.github.com>
2018-06-06 14:54:53 +0700

    update adblock

--- public/themes/js/affiliate/withdrawal/withdrawal.js
+++ public/themes/js/affiliate/withdrawal/withdrawal.js
@@ -1,57 +1,60 @@
-$(document).ready(() => {
+var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    var th = ['', 'thousand', 'million', 'billion', 'trillion'];
-    var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-    function toWords(s) {
-        s = s.toString();
-        s = s.replace(/[\, ]/g, '');
-        if (s != parseFloat(s)) return 'not a number';
-        var x = s.indexOf('.');
-        if (x == -1)
-            x = s.length;
-        if (x > 15)
-            return 'too big';
-        var n = s.split('');
-        var str = '';
-        var sk = 0;
-        for (var i = 0; i < x; i++) {
-            if ((x - i) % 3 == 2) {
-                if (n[i] == '1') {
-                    str += tn[Number(n[i + 1])] + ' ';
-                    i++;
-                    sk = 1;
-                } else if (n[i] != 0) {
-                    str += tw[n[i] - 2] + ' ';
-                    sk = 1;
-                }
-            } else if (n[i] != 0) { // 0235
-                str += dg[n[i]] + ' ';
-                if ((x - i) % 3 == 0) str += 'hundred ';
+function toWords(s) {
+    s = s.toString();
+    s = s.replace(/[\, ]/g, '');
+    if (s != parseFloat(s)) return 'not a number';
+    var x = s.indexOf('.');
+    if (x == -1)
+        x = s.length;
+    if (x > 15)
+        return 'too big';
+    var n = s.split('');
+    var str = '';
+    var sk = 0;
+    for (var i = 0; i < x; i++) {
+        if ((x - i) % 3 == 2) {
+            if (n[i] == '1') {
+                str += tn[Number(n[i + 1])] + ' ';
+                i++;
+                sk = 1;
+            } else if (n[i] != 0) {
+                str += tw[n[i] - 2] + ' ';
                 sk = 1;
             }
-            if ((x - i) % 3 == 1) {
-                if (sk)
-                    str += th[(x - i - 1) / 3] + ' ';
-                sk = 0;
-            }
+        } else if (n[i] != 0) { // 0235
+            str += dg[n[i]] + ' ';
+            if ((x - i) % 3 == 0) str += 'hundred ';
+            sk = 1;
         }
-
-        if (x != s.length) {
-            var y = s.length;
-            str += 'point ';
-            for (var i = x + 1; i < y; i++)
-                str += dg[n[i]] + ' ';
+        if ((x - i) % 3 == 1) {
+            if (sk)
+                str += th[(x - i - 1) / 3] + ' ';
+            sk = 0;
         }
-        return str.replace(/\s+/g, ' ');
     }
 
-    function IsNumeric(input) {
-        return (input - 0) == input && ('' + input).trim().length > 0;
+    if (x != s.length) {
+        var y = s.length;
+        str += 'point ';
+        for (var i = x + 1; i < y; i++)
+            str += dg[n[i]] + ' ';
     }
+    return str.replace(/\s+/g, ' ');
+}
 
+function IsNumeric(input) {
+    return (input - 0) == input && ('' + input).trim().length > 0;
+}
+
+
+
+
+
+$(document).ready(() => {
     var amount = $("#amount-withdrawal");
     var currency = "USD";
     var method = "paypal";
@@ -87,8 +90,6 @@ $(document).ready(() => {
         });
         b.attr("src", "");
     }
-
-
 
 
     $('.select-product').click((event) => {

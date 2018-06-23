d953f79ffa4ac5a8f4fcf770e74fc5cdb160b97b
lehieu1312 <hieulm1312@gmail.com>
2018-06-23 11:34:58 +0700

    test datetimezone 2

--- public/themes/js/admin/datetime.js
+++ public/themes/js/admin/datetime.js
@@ -1,11 +1,3 @@
 $(document).ready(() => {
-    var toLocalTime = function(time) {
-        var d = new Date(time);
-        console.log(d);
-        var offset = (new Date().getTimezoneOffset() / 60) * -1;
-        console.log(offset);
-        var n = new Date(d.getTime() + offset);
-        console.log(n);
-        return n;
-    };
+
 })

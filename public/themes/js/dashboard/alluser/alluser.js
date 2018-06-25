38f0d1f63a9e38f3a833c4c776276657db2c4625
namthang <namthang161295@users.noreply.github.com>
2018-06-25 15:05:04 +0700

    fix speed alluser

--- public/themes/js/dashboard/alluser/alluser.js
+++ public/themes/js/dashboard/alluser/alluser.js
@@ -1,20 +1,13 @@
 $(document).ready(() => {
     var istest_client = document.getElementsByClassName('value-is-test');
     var idUser = document.getElementsByClassName('value-id-user');
+    var status_test = document.getElementsByClassName("status-test");
     $(".btn-to-test").each(function (i) {
         var istest;
         $(this).click(() => {
             if (istest_client[i].value == "true") {
-                $(this).addClass("btn-deploy");
-                $(this).removeClass("btn-del-to-test");
-                $(this).text("Add to Test");
-                istest_client[i].value = "false";
                 istest = false;
             } else {
-                $(this).addClass("btn-del-to-test");
-                $(this).removeClass("btn-deploy");
-                $(this).text("Delete to Test");
-                istest_client[i].value = "true";
                 istest = true;
             }
             $.ajax({
@@ -30,6 +23,20 @@ $(document).ready(() => {
                         console.log("ok");
                         // window.location.href = "/dashboard/notification/alluser/" + $("#idapp-using").val()
                     }
+                }
+            }).always(()=>{
+                if (istest_client[i].value == "true") {
+                    $(this).addClass("btn-deploy");
+                    $(this).removeClass("btn-del-to-test");
+                    $(this).text("Add to Test");
+                    status_test[i].text = "no";
+                    istest_client[i].value = "false";
+                } else {
+                    $(this).addClass("btn-del-to-test");
+                    $(this).removeClass("btn-deploy");
+                    $(this).text("Delete to Test");
+                    status_test[i].text = "yes";
+                    istest_client[i].value = "true";
                 }
             })
         })

cbad1a30bab8508f9aaefd5f8841da575a40a0db
lehieu1312 <hieulm1312@gmail.com>
2018-06-28 13:37:52 +0700

    fix noti user status

--- routes/admin/membership/index.js
+++ routes/admin/membership/index.js
@@ -54,7 +54,7 @@ router.post('/send-noti-to-user', checkAdmin, multipartMiddleware, (req, res) =>
                 content: req.body.content,
                 dateCreate: sDate,
                 statusNoti: false,
-                status: true
+                status: false
             });
             return notificationUserData.save().then(() => {
                 return sendNotiUserModels.find({

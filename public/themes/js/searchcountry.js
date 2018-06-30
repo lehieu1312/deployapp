b8604590633656d7c2b476f3902ef10772d7ae99
namthang <namthang161295@users.noreply.github.com>
2018-06-30 11:04:16 +0700

    update search

--- public/themes/js/searchcountry.js
+++ public/themes/js/searchcountry.js
@@ -4,9 +4,11 @@ $(document).ready(() => {
         arrcountry.push($(this).text()[0].toUpperCase())
     })
     $(document).keypress(function (e) {
-        if (document.getElementById("country1").style.display != "none") {
-            var control = arrcountry.indexOf(e.originalEvent.key.toUpperCase());
-            document.getElementById("country1").scrollTop = control * 34;
+        if(document.getElementById("country1")){
+            if (document.getElementById("country1").style.display != "none") {
+                var control = arrcountry.indexOf(e.originalEvent.key.toUpperCase());
+                document.getElementById("country1").scrollTop = control * 34;
+            }
         }
     });
 
